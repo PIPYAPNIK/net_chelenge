a36b9908b023b75e33c0ea1a599d143494c56be3
PIPYAPNIK <malanin16@mail.ru>
2019-09-22 11:46:05 +0300

    My first commit

---
+++ ConvolutionWpf/ConvolutionWpf/Command/ContrastCommand.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Catel.MVVM;
+
+namespace ConvolutionWpf.Commands
+{
+    public class ContrastCommand : Command
+    {
+        private readonly Func<WriteableBitmap> _imageFactory;
+
+        public ContrastCommand(Func<WriteableBitmap> imageFactory)
+            : base(() => { })
+        {
+            _imageFactory = imageFactory;
+        }
+
+        public void ExecuteCommand()
+        {
+            var image = _imageFactory();
+            if (image == null)
+                return;
+
+            var pixels = new byte[image.PixelHeight * image.BackBufferStride];
+            image.CopyPixels(pixels, image.BackBufferStride, 0);
+
+            var resultPixels = new byte[image.PixelHeight * image.BackBufferStride];
+
+            //int index = j * image.BackBufferStride + 4 * i;
+            //todo
+
+            int averageValue = 0;
+            int result = 0;
+
+            for (int i = 0; i < image.PixelWidth; ++i)
+            {
+                for (int j = 0; j < image.PixelHeight; ++j)
+                {
+                    int index = j * image.BackBufferStride + 4 * i;
+
+                    double red = pixels[index];
+                    double green = pixels[index + 1];
+                    double blue = pixels[index + 2];
+
+                    averageValue += (int)(red * 0.299 + green * 0.587 + blue * 0.114);
+                    result = averageValue / (image.PixelHeight * image.PixelWidth);
+
+                    for (int c = 0; c < 3; ++c)
+                    {
+                        resultPixels[index + c] = (byte)result;
+                    }
+
+                    resultPixels[index + 3] = pixels[index + 3];
+                }
+            }
+
+            image.WritePixels(new Int32Rect(0, 0, image.PixelWidth, image.PixelHeight), resultPixels, image.BackBufferStride, 0);
+        }
+
+        protected override void Execute(object parameter, bool ignoreCanExecuteCheck)
+        {
+            ExecuteCommand();
+        }
+    }
+}
---
+++ ConvolutionWpf/ConvolutionWpf/Command/FlipCommand.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Catel.MVVM;
+
+namespace ConvolutionWpf.Commands
+{
+    public class FlipCommand : Command
+    {
+        private readonly Func<WriteableBitmap> _imageFactory;
+
+        public event Action<WriteableBitmap> OnImageChanged;
+
+        public FlipCommand(Func<WriteableBitmap> imageFactory)
+            : base(() => { })
+        {
+            _imageFactory = imageFactory;
+        }
+
+        public void ExecuteCommand()
+        {
+            var image = _imageFactory();
+            if (image == null)
+                return;
+
+            var pixels = new byte[image.PixelHeight * image.BackBufferStride];
+            image.CopyPixels(pixels, image.BackBufferStride, 0);
+
+            var imageRes = new WriteableBitmap(2 * image.PixelWidth, image.PixelHeight, image.DpiX, image.DpiY, image.Format, image.Palette);
+            var resultPixels = new byte[imageRes.PixelHeight * imageRes.BackBufferStride];
+            var indexRes = 0;
+
+    
+            for (int i = 0; i < image.PixelWidth; ++i)
+            {
+                for (int j = 0; j < image.PixelHeight; ++j)
+                {
+                    int indexRead = j * image.BackBufferStride + 4 * i;
+                    int indexWrite = j * imageRes.BackBufferStride + 4 * i;
+
+                    for (int c = 0; c < 4; ++c)
+                    {
+                        resultPixels[indexWrite + c] = pixels[indexRead + c];
+                        indexRes++;
+                    }
+                }
+            }
+
+            for (int i = image.PixelWidth - 1; i > 0; --i)
+            {
+                for (int j = image.PixelHeight - 1; j > 0; --j)
+                {
+                    int indexRead = j * image.BackBufferStride + 4 * i;
+                    int indexWrite = j * imageRes.BackBufferStride + imageRes.BackBufferStride - 4 * i;
+
+                    for (int c = 0; c < 4; ++c)
+                    {
+                        resultPixels[indexWrite + c] = pixels[indexRead + c];
+                        indexRes++;
+                    }
+                }
+            }
+
+            imageRes.WritePixels(new Int32Rect(0, 0, imageRes.PixelWidth, imageRes.PixelHeight), resultPixels, imageRes.BackBufferStride, 0);
+
+            OnImageChanged?.Invoke(imageRes);
+        }
+
+        protected override void Execute(object parameter, bool ignoreCanExecuteCheck)
+        {
+            ExecuteCommand();
+        }
+    }
+}
