950d825df7cae4104e05d4c359f1621d57773893
PIPYAPNIK <malanin16@mail.ru>
2019-09-22 15:33:31 +0300

    Перевод двумерного в одномерный готов

---
+++ ArrayInArray/ArrayInArray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ArrayInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrayTwoDimensionalWidth = 3;
+            int arrayTwoDimensionalHeight = 3;
+            int count = 0;
+
+            string[,] arrayTwoDimensional = new string[arrayTwoDimensionalWidth, arrayTwoDimensionalHeight];
+            string[] arrayOneDimensional = new string[arrayTwoDimensionalWidth * arrayTwoDimensionalHeight];
+
+            for (int i = 0; i < arrayTwoDimensionalWidth; i++)
+            {
+                for (int j = 0; j < arrayTwoDimensionalHeight; j++)
+                {
+                    arrayTwoDimensional[i, j] = $"{(i, j)}";
+                    Console.Write($"{arrayTwoDimensional[i, j]} ");
+                    arrayOneDimensional[count] = arrayTwoDimensional[i, j];
+                    count++;
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("");
+
+            for (int i = 0; i < arrayOneDimensional.Length; i++)
+            {
+                Console.Write($"{arrayOneDimensional[i]} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
