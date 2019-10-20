de144ef54c621955df4a5f331e153f1630e08328
PIPYAPNIK <malanin16@mail.ru>
2019-10-20 18:40:20 +0300

    Сделал 2 дз

--- VeryHardHomeWork/MyEvent/Program.cs
+++ VeryHardHomeWork/MyEvent/Program.cs
@@ -5,11 +5,39 @@ namespace MyEvent
 {
     class Program
     {
+        private static void OnSerch(object sender, string rez)
+        {
+            Console.WriteLine(rez);
+        }
         static void Main(string[] args)
         {
-            string path = @"C:\Test";
-           
-            Console.ReadKey();
+            TextSerch textSerch = new TextSerch();
+            textSerch.Serch += OnSerch;
+            textSerch.Start(@"C:\Test\", "C#");
+            Console.ReadLine();
+        }
+
+    }
+    class TextSerch
+    {
+        public event EventHandler<string> Serch;
+
+        public void Start(string path, string text)
+        {
+            string[] Files = Directory.GetFiles(path);
+            foreach (string i in Files)
+            {
+                int line = 1;
+                foreach (string j in File.ReadLines(i))
+                {
+                    if (j.Contains(text))
+                    {
+                        Serch?.Invoke(this, $"Текст найден в файле {i} номер строки - {line}");
+                    }
+
+                    ++line;
+                }
+            }
         }
     }
 }
