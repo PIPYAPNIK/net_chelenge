aa433ad83af20e30a3a5561e831485154f1192bc
PIPYAPNIK <malanin16@mail.ru>
2019-10-26 20:17:38 +0300

    Доп дз по ивентам

---
+++ EventGame/ConsoleApp2/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Player
+    {
+        public event Action <int, int, int> BallIsReflected;
+        public event Action BallIsNotReflected;
+        public int x;
+        public int y;
+        public int deg;
+
+        public void TryReflectBall (bool randEvent)
+        {
+            if (randEvent)
+                BallIsReflected?.Invoke(x, y, deg);
+            else
+                BallIsNotReflected?.Invoke();
+        }
+    }
+}
---
+++ EventGame/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+            bool[] arr = { true, false };
+
+            Player playerForst = new Player();
+            Player playerSecond = new Player();
+            playerForst.BallIsReflected += Win;
+            playerForst.BallIsNotReflected += Lose;
+            playerForst.x = RandomyzerValue();
+            playerForst.y = RandomyzerValue();
+            playerForst.deg = RandomyzerValue();
+            playerForst.TryReflectBall(arr[rd.Next(arr.Length)]);
+        }
+
+        public static int RandomyzerValue()
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            Random rand = new Random();
+            return array[rand.Next(array.Length)];
+        }
+        public static void Win(int x, int y, int deg)
+        {
+            Console.WriteLine($"Пытаюсь поймать мячик, отразил в точке ({x}, {y}), мяик отлетел под углом -  {deg}");
+        }
+
+        public static void Lose()
+        {
+            Console.WriteLine("Вы проиграли");
+        }
+    }
+}
