a04d974195576f642b9dc6b5c6f2bcc6dec3c34b
PIPYAPNIK <malanin16@mail.ru>
2019-10-27 18:06:03 +0300

    Добавил второго игрока

--- EventGame/ConsoleApp2/Player.cs
+++ EventGame/ConsoleApp2/Player.cs
@@ -1,23 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace ConsoleApp2
 {
     class Player
     {
-        public event Action <int, int, int> BallIsReflected;
-        public event Action BallIsNotReflected;
+        public event Action <int, int, int, string> BallIsReflected;
+        public event Action <string> BallIsNotReflected;
         public int x;
         public int y;
         public int deg;
+        public string playerName;
 
         public void TryReflectBall (bool randEvent)
         {
             if (randEvent)
-                BallIsReflected?.Invoke(x, y, deg);
+                BallIsReflected?.Invoke(x, y, deg, playerName);
             else
-                BallIsNotReflected?.Invoke();
+                BallIsNotReflected?.Invoke(playerName);
+            Thread.Sleep(500);
+        }
+
+        public void TryWin(int x, int y, int deg, string name)
+        {
+            Console.WriteLine($"{name}: пытаюсь поймать мячик, отразил в точке ({x}, {y}), мяик отлетел под углом -  {deg}");
+        }
+
+        public void Lose(string name)
+        {
+            Console.WriteLine($"{name}: вы проиграли");
+            System.Environment.Exit(0);
         }
     }
 }
--- EventGame/ConsoleApp2/Program.cs
+++ EventGame/ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleApp2
 {
@@ -7,16 +8,36 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Random rd = new Random();
-            bool[] arr = { true, false };
+            bool[] arr = { true, false, true, true, true, true };
 
-            Player playerForst = new Player();
+            Player playerFirst = new Player();
+            playerFirst.BallIsReflected += playerFirst.TryWin;
+            playerFirst.BallIsNotReflected += playerFirst.Lose;
+
             Player playerSecond = new Player();
-            playerForst.BallIsReflected += Win;
-            playerForst.BallIsNotReflected += Lose;
-            playerForst.x = RandomyzerValue();
-            playerForst.y = RandomyzerValue();
-            playerForst.deg = RandomyzerValue();
-            playerForst.TryReflectBall(arr[rd.Next(arr.Length)]);
+            playerSecond.BallIsReflected += playerSecond.TryWin;
+            playerSecond.BallIsNotReflected += playerSecond.Lose;
+
+            bool loop = true;
+            while (loop)
+            {
+                loop = arr[rd.Next(arr.Length)];
+
+                playerFirst.x = RandomyzerValue();
+                playerFirst.y = RandomyzerValue();
+                playerFirst.deg = RandomyzerValue();
+                playerFirst.playerName = "First player";
+
+                playerSecond.x = RandomyzerValue();
+                playerSecond.y = RandomyzerValue();
+                playerSecond.deg = RandomyzerValue();
+                playerSecond.playerName = "Second player";
+
+                playerFirst.TryReflectBall(arr[rd.Next(arr.Length)]);
+                playerSecond.TryReflectBall(arr[rd.Next(arr.Length)]);
+
+                if (loop == false) playerFirst.TryReflectBall(loop);
+            }
         }
 
         public static int RandomyzerValue()
@@ -24,15 +45,6 @@ namespace ConsoleApp2
             int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
             Random rand = new Random();
             return array[rand.Next(array.Length)];
-        }
-        public static void Win(int x, int y, int deg)
-        {
-            Console.WriteLine($"Пытаюсь поймать мячик, отразил в точке ({x}, {y}), мяик отлетел под углом -  {deg}");
-        }
-
-        public static void Lose()
-        {
-            Console.WriteLine("Вы проиграли");
         }
     }
 }
