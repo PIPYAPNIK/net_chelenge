3c0384cb35b4305a2c4d70f9352318cf9be6e854
PIPYAPNIK <malanin16@mail.ru>
2019-10-27 18:49:41 +0300

    Дз по комплексным числам

---
+++ Complex(OOP)/Complex(OOP)/Complex.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Complex_OOP_
+{
+    class Complex
+    {
+        private double actual {get; set;}
+        private double imaginary { get; set; }
+
+        public void SetValue(double actual, double imaginary)
+        {
+            this.actual = actual;
+            this.imaginary = imaginary;
+        }
+
+        public static void Addition (Complex first, Complex second)
+        {
+            Console.WriteLine(first.actual + second.actual + ", " + first.imaginary + second.imaginary + "i");
+        }
+
+        public static void Subtraction(Complex first, Complex second)
+        {
+            Console.WriteLine($"{first.actual - second.actual}, {first.imaginary - second.imaginary}i");
+        }
+
+        public static void Multiplication(Complex first, Complex second)
+        {
+            Console.WriteLine($"{first.actual * second.actual - first.imaginary * second.imaginary}, {first.actual * second.imaginary - second.actual * first.imaginary}i");
+        }
+    }
+}
---
+++ Complex(OOP)/Complex(OOP)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Complex_OOP_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex itemFirst = new Complex();
+            Complex itemSecond = new Complex();
+            Complex action = new Complex();
+
+            itemFirst.SetValue(18, 29);
+            itemSecond.SetValue(21, 5);
+
+            Complex.Addition(itemFirst, itemSecond);
+            Complex.Subtraction(itemFirst, itemSecond);
+            Complex.Multiplication(itemFirst, itemSecond);
+
+            Console.ReadKey();
+        }
+    }
+}
